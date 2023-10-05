1e918b600c0eb1da57044f212a33bd986540f234
mariadudasn <maria.santos081207@gmail.com>
2023-10-05 15:34:54 -0300

    first commit

---
+++ Controller.py
@@ -0,0 +1,46 @@
+from Model import *
+
+class ControllerAdicionarTarefa():
+    def __init__(self, tarefa):
+        m = 0
+        while m == 0:
+            try:
+                self.tarefa = tarefa
+                
+                if self.tarefa == "":
+                    print("Informe uma tarefa válida")
+                else:
+                    if TODO.AdicionarTarefa(self.tarefa) == True:
+                        print ("Tarefa adicionada")
+                        m = 1
+                    else:
+                        print ("Algum problema foi encontrado ao tentar adicionar tarefa")
+            except Exception:
+                print("Opção inválida")
+
+class ControllerExcluirTarefa():
+    def __init__(self, excluir):
+        s = 0
+        while s == 0:
+            try:
+                x = int(excluir)
+                self.excluir = x - 1
+
+                if TODO.ExcluirTarefa(self.excluir) == True:
+                        print ("Tarefa excluída")
+                        s = 1
+                else:
+                    print ("Algum problema foi encontrado ao tentar excluir tarefa")
+
+            except Exception:
+                print("Opção inválida")
+                excluir = input ("Qual o índice da tarefa que deseja excluir? ")
+
+
+class ControllerListarTarefa():
+    def __init__(self):
+        ControllerLista = TODO.ListarTarefa()
+        cont = 1
+        for tarefas in ControllerLista:
+            print (f"{cont} -- {tarefas}")
+            cont += 1
---
+++ View.py
@@ -0,0 +1,50 @@
+from Controller import*
+import os
+
+sair = 0
+while sair == 0:
+    os.system("cls")
+    print ("SOFTWARE DE TO-DO")
+    print ("01 -> ADICIONAR TAREFA")
+    print ("02 -> LISTAR TAREFAS")
+    print ("03 -> EXCLUIR TAREFA")
+    print ("04 -> SAIR")
+    print (" ")
+    print("QUAL OPÇÃO DESEJA?")
+    menu = input(">> ")
+    os.system("pause")
+
+    match menu:
+        case "1":
+            os.system("cls")
+            tarefa = input ("Adicione uma tarefa: ")
+            adicionarTarefa = ControllerAdicionarTarefa(tarefa)
+            print (" ")
+            os.system("pause")
+        case "2":
+            os.system("cls")
+            print ("Lista de tarefas:")
+            print(" ")
+            listarTarefa = ControllerListarTarefa()
+            print (" ")
+            os.system("pause")
+        case "3":
+            os.system("cls")
+            print ("Lista de tarefas:")
+            print(" ")
+            listarTarefa = ControllerListarTarefa()
+            print(" ")
+            excluir = input ("Qual o índice da tarefa que deseja excluir? ")
+            excluirTarefa = ControllerExcluirTarefa(excluir)
+            print(" ")
+            print ("Lista de tarefas:")
+            print(" ")
+            listarTarefa = ControllerListarTarefa()
+            os.system("pause")
+        case "4":
+            print ("Saindo...")
+            os.system("pause")
+            break
+        case _:
+            print ("Opção inválida")
+            os.system("pause")
