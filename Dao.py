a6698a0d910322808e77e98df78e6bd08444844b
mariadudasn <maria.santos081207@gmail.com>
2023-10-19 14:33:51 -0300

    Update

--- Dao.py
+++ Dao.py
@@ -1,11 +1,20 @@
+from random import randint
+
 Arquivo = "ToDo.txt"
+ids = []
 
 class DAO_AdicionarTarefa:
 
     def AdicionarTarefa(self,tarefa):
+        self.id = randint(1000,9999)
+        ids.append(self.id)
         with open(Arquivo, "a") as arquivo:
-            arquivo.write(f"{tarefa}\n")
-        return True
+            id = randint(1000,9999)
+            if id in ids:
+                return False
+            else:
+                arquivo.write(f"{id}\t{tarefa}\n")
+                return True
 
 class DAO_ListarTarefa:
 
